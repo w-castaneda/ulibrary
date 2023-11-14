1afe65923ac433f8ea945db4f838040945c08272
Wilfredo Castaneda <wilfredo.castaneda.guerrero@gmail.com>
2023-11-14 15:38:12 -0600

    ULIBRARY - Initial Configuration
    Add pom.xml and its dependencies in order to setup the project.

---
+++ src/main/java/com/ito/ulibrary/UlibraryApplication.java
@@ -0,0 +1,13 @@
+package com.ito.ulibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UlibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UlibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ito/ulibrary/UlibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ito.ulibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UlibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
